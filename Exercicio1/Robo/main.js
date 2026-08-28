7d006d28b25cd0a31fe772c0a9fb8aaa6b1aaf06
Pedro Bako <57959979+pedrobako@users.noreply.github.com>
2026-08-28 16:11:58 -0300

    Colorindo e melhorando os comentários
    Added vertex and color buffers for trunk, head, arms, and legs. Updated shaders to handle color attributes.

--- Exercicio1/Robo/main.js
+++ Exercicio1/Robo/main.js
@@ -5,11 +5,12 @@ const canvas = document.getElementById("canvas");
 //Criando a variável que será a interface com a API WebGL
 const gl = canvas.getContext("webgl2");
 
+//Lançando o erro caso não consiga obter a interface com a API WebGL
 if (!gl){
 	throw new Error("WebGL 2 não é suportado.");
 }
 
-// 1. VÉRTICES
+// VÉRTICES
 const verticesTronco = new Float32Array([
      0.3,  0.5,
     -0.3,  0.5,
@@ -52,38 +53,93 @@ const verticesPernas = new Float32Array([
     0.1, -0.8
 ])
 
-// 2. BUFFERS
-// Declarando o buffer (criando um espaço na memória)
-const verticesBuffer = gl.createBuffer();
+// DEFININDO AS CORES
+const coresTronco = new Float32Array([
+    // Verde nos 4 vértices
+    0.0, 1.0, 0.0, 1.0,
+    0.0, 1.0, 0.0, 1.0,
+    0.0, 1.0, 0.0, 1.0,
+    0.0, 1.0, 0.0, 1.0,
+]);
 
-// Vinculando o buffer criado com o buffer alvo do webGL
-gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+const coresCabeca = new Float32Array([
+    // Azul nos 4 vértices
+    0.0, 0.0, 1.0, 1.0,
+    0.0, 0.0, 1.0, 1.0,
+    0.0, 0.0, 1.0, 1.0,
+    0.0, 0.0, 1.0, 1.0,
+]);
 
-// Enviando os dados dos vértices para o buffer
-gl.bufferData(
-	gl.ARRAY_BUFFER,
-	verticesTronco,
-	gl.STATIC_DRAW
-);
+const coresBracos = new Float32Array([
+    // Amarelo nos 4 vértices do braço esquerdo
+    1.0, 1.0, 0.0, 1.0,
+    1.0, 1.0, 0.0, 1.0,
+    1.0, 1.0, 0.0, 1.0,
+    1.0, 1.0, 0.0, 1.0,
 
-// 3. VERTEX SHADER
-const vertexShaderSource = `#version 300 es
-in vec2 aPosition;
+    // Amarelo nos 4 vértices do braço direito
+    1.0, 1.0, 0.0, 1.0,
+    1.0, 1.0, 0.0, 1.0,
+    1.0, 1.0, 0.0, 1.0,
+    1.0, 1.0, 0.0, 1.0,
+]);
 
-void main(){
-	gl_Position = vec4(aPosition, 0.0, 1.0);
-}`;
+const coresPernas = new Float32Array([
+    // Vermelho nos 4 vértices da perna esquerda
+    1.0, 0.0, 0.0, 1.0,
+    1.0, 0.0, 0.0, 1.0,
+    1.0, 0.0, 0.0, 1.0,
+    1.0, 0.0, 0.0, 1.0,
 
-// 4. FRAGMENT SHADER
-const fragmentShaderSource = `#version 300 es
+    // Vermelho nos 4 vértices da perna direita
+    1.0, 0.0, 0.0, 1.0,
+    1.0, 0.0, 0.0, 1.0,
+    1.0, 0.0, 0.0, 1.0,
+    1.0, 0.0, 0.0, 1.0,
+]);
+
+// DEFININDO O CÓDIGO DO VERTEX SHADER
+const vertexShaderSource = `#version 300 es //definindo a versão 3.00 da linguagem GLSL ES para este shader
+
+	//declarando uma varável de entrada de dados do tipo vetor float de duas posições
+	in vec2 aPosition;
+
+	//declarando um vetor de entrada de 4 posições para receber o atributo de cor
+	in vec4 aColor;
+
+	//declarando um vetor de saída de 4 posições para transmitir o atributo de cor para o fragmentshader
+	out vec4 vColor;
+
+	void main(){
+		//gl_Position é uma variável pré-definida(built-in) do GLSL, ela define onde o vértice será colocado
+		//Ele recebe 4 valores (x, y, z, w) onde w está relacionado com transformações geométricas e perspectivas
+		//vec4 é um tipo que indica um vetor com 4 componentes floats
+		gl_Position = vec4(
+							aPosition,//variável de entrada que repassará duas das 4 posições do vetor (x e y do vértice)
+							0.0,//posição z do vértice, neste caso é zero, pois estamos trabalhando em 2D
+							1.0//posição w relacionada com transformações geométricas e perspectivas
+							);
+		
+		vColor = aColor;
+	}`;
+
+// DEFININDO O CÓDIGO DO FRAGMENT SHADER
+const fragmentShaderSource = `#version 300 es //defindo a versão 3.00 da linguagem GLSL ES para este shader
+	//setando precisão média para o float que será utilizado
 	precision mediump float;
+	
+	//declarando uma varável de entrada de dados do tipo vetor float de 4 posições que receberá os dados de cor do vertexshader
+	in vec4 vColor;
+
+	//declarando uma varável de saída de dados do tipo vetor float de 4 posições
 	out vec4 outColor;
 
 	void main(){
-		outColor = vec4(0.5, 0.5, 0.5, 1.0);
+		//outColor = vec4(0.5, 0.5, 0.5, 1.0); //atribuindo os valores RGBA do vetor de saída do FS, onde A é "Alpha" transparência/opacidade
+		outColor = vColor;
 	}`;
 
-// 5. COMPILAR SHADERS
+// COMPILANDO OS SHADERS
 function createShader(gl, type, source){
 	const shader = gl.createShader(type);
 	gl.shaderSource(shader, source);
@@ -96,12 +152,10 @@ function createShader(gl, type, source){
 	}
 	return shader;
 }
-
 const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-
 const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 
-// 6. CRIAR O PROGRAMA
+// CRIANDO O PROGRAMA
 const program = gl.createProgram();
 gl.attachShader(program, vertexShader);
 gl.attachShader(program, fragmentShader);
@@ -111,67 +165,152 @@ if(!gl.getProgramParameter(program, gl.LINK_STATUS)){
 	throw new Error(gl.getProgramInfoLog(program));
 }
 
-// 7. LOCAL DO ATRIBUTO
+// CONFIGURANDO O BUFFER
+// Declarando o buffer de vértices
+const verticesBuffer = gl.createBuffer();
+
+gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+
+// Obtendo o local(índice) do atributo posição(aPosition) no VertexShader
 const positionLocation = gl.getAttribLocation(program, "aPosition");
 
-// 8. CONFIGURAR O ATRIBUTO
-gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+// Habilitando o aPosition para receber dados a partir de um array
 gl.enableVertexAttribArray(positionLocation);
-gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
 
-// 9. LIMPAR TELA
-gl.clearColor(0.1, 0.1, 0.1, 1.0);
-gl.clear(gl.COLOR_BUFFER_BIT);
-
-// 10. o tronco
-gl.useProgram(program);
-gl.drawArrays(
-	gl.TRIANGLE_FAN, //modo forme uma sequência de triângulos usando o 1º vértice como vértice comum.
-	0, //índice do 1º vértice
-	4); //Qtd de vértices que serão utilizados
-
-// Desenhar a cabeça
-gl.bufferData(
-	gl.ARRAY_BUFFER,
-	verticesCabeca,
-	gl.STATIC_DRAW
+// Definindo como os dados oriundos do Array de vértices serão interpretados
+gl.vertexAttribPointer(
+	positionLocation, //onde deverão ser entregues os dados do array de vértices
+	2, //quantidade de valores por vértice
+	gl.FLOAT, //tipo de dados do array de vértices (Float32Array combina com gl.FLOAT)
+	false, //diz se o WebGL deve normalizar os valores antes de entregá-los ao Vertex Shader
+	0, //salto entre o final dos dados de um vértice para os dados do próximo vértice
+	0 //salto para início da leitura do 1º vértice
 );
 
-gl.useProgram(program);
-gl.drawArrays(
-	gl.TRIANGLE_FAN, //modo forme uma sequência de triângulos usando o 1º vértice como vértice comum.
-	0, //índice do 1º vértice
-	4); //Qtd de vértices que serão utilizados
+// Declarando o buffer de cores
+const coresBuffer = gl.createBuffer();
 
-// Desenhar os braços
-// Preparando o buffer
+gl.bindBuffer(gl.ARRAY_BUFFER, coresBuffer);
+
+// Obtendo o local(indice) do atributo cor no FragmentShader
+const colorLocation = gl.getAttribLocation(program, "aColor");
+
+//Habilitando o aColor para receber dados a partir de uma array
+gl.enableVertexAttribArray(colorLocation);
+
+// Definindo como os dados oriundos do Array de cores serão interpretados
+gl.vertexAttribPointer(
+	colorLocation, //onde deverão ser entregues os dados do array de vértices
+	4, //quantidade de valores por vértice
+	gl.FLOAT, //tipo de dados do array de vértices (Float32Array combina com gl.FLOAT)
+	false, //diz se o WebGL deve normalizar os valores antes de entregá-los ao Vertex Shader
+	0, //salto entre o final dos dados de um vértice para os dados do próximo vértice
+	0 //salto para início da leitura do 1º vértice
+);
+
+// DEFINIR O PROGRAMA QUE SERÁ USADO, QUE JÁ ESTÁ TOTALMENTE CONFIGURADO COM O VERTEXSHADER E FRAGMENTSHADER
+gl.useProgram(program);
+
+// LIMPAR TELA
+gl.clearColor(0.1, 0.1, 0.1, 1.0);//Define a cor que "limpará" a tela
+gl.clear(gl.COLOR_BUFFER_BIT);//Efetivamente aplica a cor selecionada em todo o canvas
+
+// Desenhar os braços (Desenhando os braços antes do tronco para que eles fiquem "por trás")
+// Vinculando o buffer com o buffer dos vértices
+gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+// Enviando os dados para o buffer
 gl.bufferData(
 	gl.ARRAY_BUFFER,
 	verticesBracos,
 	gl.STATIC_DRAW
 );
 
-// Iniciando o programa
-gl.useProgram(program);
+// Vinculando o buffer com o buffer das cores
+gl.bindBuffer(gl.ARRAY_BUFFER, coresBuffer);
+// Enviando os dados para o buffer
+gl.bufferData(
+	gl.ARRAY_BUFFER,
+	coresBracos,
+	gl.STATIC_DRAW
+);
 
 // Desenhando o braço esquerdo
 gl.drawArrays(
 	gl.TRIANGLE_FAN, //modo forme uma sequência de triângulos usando o 1º vértice como vértice comum.
 	0, //índice do 1º vértice
+	4 //Qtd de vértices que serão utilizados
+);
+// Desenhando o braco direito
+gl.drawArrays(gl.TRIANGLE_FAN, 4, 4);//Iniciando no índice 4 e lendo 4 vértices
+
+// Desenhando o tronco
+// Vinculando o buffer com o buffer dos vértices
+gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+// Enviando os dados para o buffer
+gl.bufferData(
+	gl.ARRAY_BUFFER,
+	verticesTronco,
+	gl.STATIC_DRAW
+);
+
+// Vinculando o buffer com o buffer das cores
+gl.bindBuffer(gl.ARRAY_BUFFER, coresBuffer);
+// Enviando os dados para o buffer
+gl.bufferData(
+	gl.ARRAY_BUFFER,
+	coresTronco,
+	gl.STATIC_DRAW
+);
+
+// Desenhando efetivamente
+gl.drawArrays(
+	gl.TRIANGLE_FAN, //modo forme uma sequência de triângulos usando o 1º vértice como vértice comum.
+	0, //índice do 1º vértice
 	4); //Qtd de vértices que serão utilizados
 
-// Desenhando o braco direito
-gl.drawArrays(gl.TRIANGLE_FAN, 4, 4);
+// Desenhando a cabeça
+// Vinculando o buffer criado com o buffer alvo do webGL
+gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+// Enviando os dados dos vértices para o buffer
+gl.bufferData(
+	gl.ARRAY_BUFFER,
+	verticesCabeca,
+	gl.STATIC_DRAW
+);
+
+// Vinculando o buffer criado com o buffer alvo do webGL
+gl.bindBuffer(gl.ARRAY_BUFFER, coresBuffer);
+// Enviando os dados dos vértices para o buffer
+gl.bufferData(
+	gl.ARRAY_BUFFER,
+	coresCabeca,
+	gl.STATIC_DRAW
+);
+
+// Desenhando efetivamente
+gl.drawArrays(
+	gl.TRIANGLE_FAN, //modo forme uma sequência de triângulos usando o 1º vértice como vértice comum.
+	0, //índice do 1º vértice
+	4); //Qtd de vértices que serão utilizados
 
 // Desenhar as pernas
-// Preparando o buffer e iniciando o programa
+// Vinculando o buffer criado com o buffer alvo do webGL
+gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+// Enviando os dados dos vértices para o buffer
 gl.bufferData(
 	gl.ARRAY_BUFFER,
 	verticesPernas,
 	gl.STATIC_DRAW
 );
 
-gl.useProgram(program);
+// Vinculando o buffer criado com o buffer alvo do webGL
+gl.bindBuffer(gl.ARRAY_BUFFER, coresBuffer);
+// Enviando os dados dos vértices para o buffer
+gl.bufferData(
+	gl.ARRAY_BUFFER,
+	coresPernas,
+	gl.STATIC_DRAW
+);
 
 // Desenhando a perna esquerda
 gl.drawArrays(
@@ -180,4 +319,4 @@ gl.drawArrays(
 	4); //Qtd de vértices que serão utilizados
 
 // Desenhando a perna direita
-gl.drawArrays(gl.TRIANGLE_FAN, 4, 4);
+gl.drawArrays(gl.TRIANGLE_FAN, 4, 4);//Iniciando no índice 4 e lendo 4 vértices
