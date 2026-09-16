73a0942792531c50c03082123ca61faee3fce4d0
Pedro Bako <57959979+pedrobako@users.noreply.github.com>
2026-09-16 14:16:09 -0300

    Versão orientada a objetos funcionando
    Refactor SceneObject and Robo classes to include movement and zoom functionalities. Update methods for movement and zooming for various body parts.

--- Exercicio4/Robo_polichinelo/main.js
+++ Exercicio4/Robo_polichinelo/main.js
@@ -85,10 +85,62 @@ class SceneObject{
 	constructor(vertices, cor){
 		this.vertices = vertices;
 		this.cor = cor;
-		this.modelTransform = m3.identity();
+		this.movimento = m3.identity();
+		this.posX = 0.0;
+		this.posY = 0.0;
+		this.zoom = 1.0;
+		this.offsetX = 0.01;
+		this.offsetZoom = 0.01;
 	}
-	atualizarTransformacao(modelTransform){
-		this.modelTransform = modelTransform;
+	
+	getPosX(){
+		return this.posX;
+	}
+	
+	setPosX(posX){
+		this.posX = posX;
+	}
+	
+	getZoom(){
+		return this.zoom;
+	}
+	
+	setZoom(zoom){
+		this.zoom = zoom;
+	}
+	
+	atualizaMovimento(){
+		this.movimento = m3.multiply(m3.scaling(this.zoom, this.zoom), m3.translation(this.posX, this.posY));
+	}
+	
+	movimentoDireita(){
+		this.posX += this.offsetX;
+		this.atualizaMovimento();
+		//this.movimento = m3.multiply(this.movimento, m3.translation(this.posX, 0));
+	}
+	
+	movimentoEsquerda(){
+		this.posX -= this.offsetX;
+		this.atualizaMovimento();
+		//this.movimento = m3.multiply(this.movimento, m3.translation(this.posX, 0));
+	}
+	
+	zoomIn(){
+		this.zoom += this.offsetZoom;
+		this.atualizaMovimento();
+		//this.movimento = m3.multiply(this.movimento, m3.scaling(this.zoom, this.zoom));
+	}
+	
+	zoomOut(){
+		this.zoom -= this.offsetZoom;
+		this.atualizaMovimento();
+		//this.movimento = m3.multiply(this.movimento, m3.scaling(this.zoom, this.zoom));
+	}
+	
+	rotacaoEmTornoDoPonto(pontoRotacao, angulo){
+		let x = pontoRotacao[0];
+		let y = pontoRotacao[1];
+		return m3.multiply(m3.translation(x, y), m3.multiply(m3.rotation(angulo), m3.translation(-x, -y)));
 	}
 }
 
@@ -104,6 +156,65 @@ class Robo extends SceneObject{
 		this.bracoDireito = bracoDireito;
 		this.pernaEsquerda = pernaEsquerda;
 		this.pernaDireita = pernaDireita;
+		this.anguloPoli = 0;
+		this.offsetPoli = 0.04;
+	}
+	
+	polichinelo(){
+		
+		if ((this.anguloPoli > Math.PI / 2) || (this.anguloPoli < - Math.PI / 4)){
+			this.offsetPoli = - this.offsetPoli;
+		}
+		
+		this.anguloPoli += this.offsetPoli;
+		
+		this.bracoEsquerdo.setAngulo(-this.anguloPoli);
+		this.bracoDireito.setAngulo(this.anguloPoli);
+		
+		// Converte o movimento do braço para o movimento da perna
+		let anguloPerna =
+			(this.anguloPoli + Math.PI/4) *
+			(Math.PI/3) /
+			(3*Math.PI/4);
+			
+		this.pernaEsquerda.setAngulo(-anguloPerna);
+		this.pernaDireita.setAngulo(anguloPerna);
+	}
+	
+	movimentoDireita(){
+		this.cabeca.movimentoDireita();
+		this.tronco.movimentoDireita();
+		this.bracoEsquerdo.movimentoDireita();
+		this.bracoDireito.movimentoDireita();
+		this.pernaDireita.movimentoDireita();
+		this.pernaEsquerda.movimentoDireita();
+	}
+	
+	movimentoEsquerda(){
+		this.cabeca.movimentoEsquerda();
+		this.tronco.movimentoEsquerda();
+		this.bracoEsquerdo.movimentoEsquerda();
+		this.bracoDireito.movimentoEsquerda();
+		this.pernaDireita.movimentoEsquerda();
+		this.pernaEsquerda.movimentoEsquerda();
+	}
+	
+	zoomIn(){
+		this.cabeca.zoomIn();
+		this.tronco.zoomIn();
+		this.bracoEsquerdo.zoomIn();
+		this.bracoDireito.zoomIn();
+		this.pernaDireita.zoomIn();
+		this.pernaEsquerda.zoomIn();
+	}
+	
+	zoomOut(){
+		this.cabeca.zoomOut();
+		this.tronco.zoomOut();
+		this.bracoEsquerdo.zoomOut();
+		this.bracoDireito.zoomOut();
+		this.pernaDireita.zoomOut();
+		this.pernaEsquerda.zoomOut();
 	}
 }
 
@@ -120,14 +231,46 @@ class Tronco extends SceneObject{
 }
 
 class Braco extends SceneObject{
-	constructor(vertices, cor){
+	constructor(vertices, cor, pontoArticulacao){
 		super(vertices, cor);
+		this.angulo = 0;
+		this.pontoArticulacao = pontoArticulacao;
+	}
+	
+	atualizaMovimento(){
+		this.movimento = m3.multiply(m3.scaling(this.zoom, this.zoom), m3.multiply(m3.translation(this.posX, this.posY), this.rotacaoEmTornoDoPonto(this.pontoArticulacao, this.angulo)));
+	}
+	
+	setAngulo(angulo){
+		this.angulo = angulo;
+		this.atualizaMovimento();
+		//this.movimento = this.rotacaoEmTornoDoPonto(this.pontoArticulacao, this.angulo);
+	}
+	
+	getAngulo(){
+		return this.angulo;
 	}
 }
 
 class Perna extends SceneObject{
-	constructor(vertices, cor){
+	constructor(vertices, cor, pontoArticulacao){
 		super(vertices, cor);
+		this.angulo = 0;
+		this.pontoArticulacao = pontoArticulacao;
+	}
+	
+	atualizaMovimento(){
+		this.movimento = m3.multiply(m3.scaling(this.zoom, this.zoom), m3.multiply(m3.translation(this.posX, this.posY), this.rotacaoEmTornoDoPonto(this.pontoArticulacao, this.angulo)));
+	}
+	
+	setAngulo(angulo){
+		this.angulo = angulo;
+		this.atualizaMovimento();
+		//this.movimento = this.rotacaoEmTornoDoPonto(this.pontoArticulacao, this.angulo);
+	}
+	
+	getAngulo(){
+		return this.angulo;
 	}
 }
 //-------------------------------------------------------
@@ -136,10 +279,10 @@ class Perna extends SceneObject{
 //-------------------------------------------------------
 const tronco = new Tronco(verticesTronco, corTronco);
 const cabeca = new Cabeca(verticesCabeca, corCabeca);
-const bracoEsquerdo = new Braco(verticesBracoEsquerdo, corBracoEsquerdo);
-const bracoDireito = new Braco(verticesBracoDireito, corBracoDireito);
-const pernaEsquerda = new Perna(verticesPernaEsquerda, corPernaEsquerda);
-const pernaDireita = new Perna(verticesPernaDireita, corPernaDireita);
+const bracoEsquerdo = new Braco(verticesBracoEsquerdo, corBracoEsquerdo, ([verticesBracoEsquerdo[0], verticesBracoEsquerdo[1]]));
+const bracoDireito = new Braco(verticesBracoDireito, corBracoDireito, ([verticesBracoDireito[0], verticesBracoDireito[1]]));
+const pernaEsquerda = new Perna(verticesPernaEsquerda, corPernaEsquerda, ([verticesPernaEsquerda[0], verticesPernaEsquerda[1]]));
+const pernaDireita = new Perna(verticesPernaDireita, corPernaDireita, ([verticesPernaDireita[0], verticesPernaDireita[1]]));
 const robo = new Robo(cabeca, tronco, bracoEsquerdo, bracoDireito, pernaEsquerda, pernaDireita);
 
 //-------------------------------------------------------
@@ -214,130 +357,6 @@ function limpaTela(r=0.1, g=0.1, b=0.1){
 }
 limpaTela();
 
-//-------------------------------------------------------
-//ANIMAÇÃO
-//-------------------------------------------------------
-let anguloBraco = 0;
-let offsetBraco = 0.04;
-let anguloPerna = 0;
-let offsetPerna = 0.02;
-let movimenta = 0;
-let zoom = 1.0;
-
-let MbracoEsquerdo;
-let MbracoDireito;
-let MpernaEsquerda;
-let MpernaDireita;
-let Mtronco;
-let Mcabeca;
-
-function rotacaoEmTornoDoPonto(x, y, angulo){
-	return m3.multiply(m3.translation(x, y), m3.multiply(m3.rotation(angulo), m3.translation(-x, -y)));
-}
-
-function atualizaAnimacao(){
-	anguloBraco += offsetBraco;
-//	anguloPerna += offsetPerna;
-	
-	//if ((anguloPerna > Math.PI/3) || (anguloPerna < 0))
-	//	offsetPerna = - offsetPerna;
-
-	if ((anguloBraco > Math.PI/2) || (anguloBraco < -Math.PI/4)){
-		offsetBraco = - offsetBraco;
-	}
-	
-	// Converte o movimento do braço para o movimento da perna
-    anguloPerna =
-        (anguloBraco + Math.PI/4) *
-        (Math.PI/3) /
-        (3*Math.PI/4);
-	
-	MbracoEsquerdo = m3.multiply(m3.scaling(zoom, zoom), m3.multiply(m3.translation(movimenta, 0), rotacaoEmTornoDoPonto(-0.3, 0.5, -anguloBraco)));
-	MbracoDireito = m3.multiply(m3.scaling(zoom, zoom), m3.multiply(m3.translation(movimenta, 0), rotacaoEmTornoDoPonto(0.3, 0.5, anguloBraco)));
-	MpernaEsquerda = m3.multiply(m3.scaling(zoom, zoom), m3.multiply(m3.translation(movimenta, 0), rotacaoEmTornoDoPonto(-0.1, -0.3, -anguloPerna)));
-	MpernaDireita = m3.multiply(m3.scaling(zoom, zoom), m3.multiply(m3.translation(movimenta, 0), rotacaoEmTornoDoPonto(0.1, -0.3, anguloPerna)));
-	Mtronco = m3.multiply(m3.scaling(zoom, zoom), m3.translation(movimenta, 0));
-	Mcabeca = m3.multiply(m3.scaling(zoom, zoom), m3.translation(movimenta, 0));
-}
-
-//-------------------------------------------------------
-//DESENHAR
-//-------------------------------------------------------
-function desenhaPernaEsquerda(){
-	gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, verticesPernaEsquerda, gl.STATIC_DRAW);
-	gl.enableVertexAttribArray(positionLocation);
-	gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-	gl.uniform3fv(colorLocation, corPernaEsquerda);
-	gl.uniformMatrix3fv(transformLocation, false, MpernaEsquerda);
-	gl.drawArrays(gl.TRIANGLE_FAN, 0, verticesPernaEsquerda.length/2);
-}
-
-function desenhaPernaDireita(){
-	gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, verticesPernaDireita, gl.STATIC_DRAW);
-	gl.enableVertexAttribArray(positionLocation);
-	gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-	gl.uniform3fv(colorLocation, corPernaDireita);
-	gl.uniformMatrix3fv(transformLocation, false, MpernaDireita);
-	gl.drawArrays(gl.TRIANGLE_FAN, 0, verticesPernaDireita.length/2);
-}
-
-function desenhaTronco(){
-	gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, verticesTronco, gl.STATIC_DRAW);
-	gl.enableVertexAttribArray(positionLocation);
-	gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-	gl.uniform3fv(colorLocation, corTronco);
-	gl.uniformMatrix3fv(transformLocation, false, Mtronco);
-	gl.drawArrays(gl.TRIANGLE_FAN, 0, verticesTronco.length/2);
-}
-
-function desenhaBracoEsquerdo(){
-	gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, verticesBracoEsquerdo, gl.STATIC_DRAW);
-	gl.enableVertexAttribArray(positionLocation);
-	gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-	gl.uniform3fv(colorLocation, corBracoEsquerdo);
-	gl.uniformMatrix3fv(transformLocation, false, MbracoEsquerdo);
-	gl.drawArrays(gl.TRIANGLE_FAN, 0, verticesBracoEsquerdo.length/2);
-}
-
-function desenhaBracoDireito(){
-	gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, verticesBracoDireito, gl.STATIC_DRAW)
-	gl.enableVertexAttribArray(positionLocation);
-	gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-	gl.uniform3fv(colorLocation, corBracoDireito);
-	gl.uniformMatrix3fv(transformLocation, false, MbracoDireito);
-	gl.drawArrays(gl.TRIANGLE_FAN, 0, verticesBracoDireito.length/2);
-}
-
-function desenhaCabeca(){
-	gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, verticesCabeca, gl.STATIC_DRAW);
-	gl.enableVertexAttribArray(positionLocation);
-	gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-	gl.uniform3fv(colorLocation, corCabeca);
-	gl.uniformMatrix3fv(transformLocation, false, Mcabeca);
-	gl.drawArrays(gl.TRIANGLE_FAN, 0, verticesCabeca.length/2);
-}
-	
-function desenhaCena(){
-	limpaTela();
-	atualizaAnimacao();
-	gl.useProgram(program);
-	desenhaPernaEsquerda();
-	desenhaPernaDireita();
-	desenhaBracoEsquerdo();
-	desenhaBracoDireito();
-	desenhaTronco();
-	desenhaCabeca();
-	
-	if (barraEspaco == true)
-		requestAnimationFrame(desenhaCena);
-}
-
 // --------------------------------------------------
 // INTERAÇÃO COM O TECLADO
 // --------------------------------------------------
@@ -352,23 +371,81 @@ function keyboardClick(event) {
         break;
 	  case ("ArrowLeft"):
         if (barraEspaco == true)
-			movimenta -= 0.01;
+			robo.movimentoEsquerda();
         break;
       case ("ArrowRight"):
         if (barraEspaco == true)
-			movimenta += 0.01;
+			robo.movimentoDireita();
         break;
 	  case ("ArrowUp"):
         if (barraEspaco == true)
-			zoom += 0.01;
+			robo.zoomIn();
         break;
       case ("ArrowDown"):
         if (barraEspaco == true)
-			zoom -= 0.01;
+			robo.zoomOut();
         break;
   }
 }
 
 window.addEventListener("keydown", keyboardClick, false);
+
+//-------------------------------------------------------
+//DESENHANDO OS OBJETOS
+//-------------------------------------------------------
+function desenhaObjeto(objeto) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
+
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        objeto.vertices,
+        gl.STATIC_DRAW
+    );
+
+    gl.enableVertexAttribArray(positionLocation);
+
+    gl.vertexAttribPointer(
+        positionLocation,
+        2,
+        gl.FLOAT,
+        false,
+        0,
+        0
+    );
+
+    gl.uniform3fv(colorLocation, objeto.cor);
+
+    gl.uniformMatrix3fv(
+        transformLocation,
+        false,
+        objeto.movimento
+    );
+
+    gl.drawArrays(
+        gl.TRIANGLE_FAN,
+        0,
+        objeto.vertices.length / 2
+    );
+}
+
+function desenhaCena() {
+    limpaTela();
+
+    gl.useProgram(program);
+
+    robo.polichinelo();
+	
+	desenhaObjeto(cabeca);
+	desenhaObjeto(bracoEsquerdo);
+	desenhaObjeto(bracoDireito);
+	desenhaObjeto(pernaEsquerda);
+	desenhaObjeto(pernaDireita);
+	desenhaObjeto(tronco);
+	
+	
+//	atualizaAnimacao();	
+	if (barraEspaco == true)
+		requestAnimationFrame(desenhaCena);
+}
 
 desenhaCena();
